e25dedf4a4dbb1cd49372554b025ab5949367843
Sachin Shastri <sachin.shastri@quantcast.com>
2021-04-22 09:59:28 -0700

    add tcfv1 and tcfv2 check for p1 and li10

--- modules/quantcastIdSystem.js
+++ modules/quantcastIdSystem.js
@@ -7,20 +7,22 @@
 
 import {submodule} from '../src/hook.js'
 import { getStorageManager } from '../src/storageManager.js';
-import { triggerPixel } from '../src/utils.js';
+import { triggerPixel, deepAccess } from '../src/utils.js';
 
 const QUANTCAST_FPA = '__qca';
 const DEFAULT_COOKIE_EXP_TIME = 392; // (13 months - 2 days)
 const PREBID_PCODE = 'p-KceJUEvXN48CE'; // Not associated with a real account
 const DOMAIN_QSERVE = 'https://pixel.quantserve.com/pixel';
 
+var emailHash;
 var cookieExpTime;
+var globalConsentData;
 
 export const storage = getStorageManager();
 
 export function firePixel() {
   // check for presence of Quantcast Measure tag _qevent obj
-  if (!window._qevents) {
+  if (!window._qevents && hasGDPRConsent(globalConsentData)) {
     let fpa = storage.getCookie(QUANTCAST_FPA);
     let fpan = '0';
     let now = new Date();
@@ -41,11 +43,37 @@ export function firePixel() {
     '&d=' + domain +
     '&et=' + et +
     '&tzo=' + tzo +
+    '&uh=' + emailHash +
+    '&uht=1' +
     '&a=' + PREBID_PCODE;
 
     triggerPixel(url);
   }
 };
+
+/**
+ * test if consent module is present, applies, and is valid for cookies (purpose 1 and legitimate interest 10)
+ * @param {ConsentData} consentData
+ * @returns {boolean}
+ */
+function hasGDPRConsent(consentData) {
+  if (consentData && typeof consentData.gdprApplies === 'boolean' && consentData.gdprApplies) {
+    if (!consentData.consentString) {
+      return false;
+    }
+    if (consentData.apiVersion === 1) {
+      if (deepAccess(consentData, 'vendorData.purposeConsents.1') === false && deepAccess(consentData, 'vendorData.purposeLegitimateInterests.10') === false) {
+        return false;
+      }
+    }   
+    if (consentData.apiVersion === 2) {
+       if (deepAccess(consentData, 'vendorData.purpose.consents.1') === false && deepAccess(consentData, 'vendorData.purpose.LegitimateInterests.10') === false) {
+         return false;
+       }
+    }
+  }
+  return true;
+}
 
 /** @type {Submodule} */
 export const quantcastIdSubmodule = {
@@ -69,13 +97,16 @@ export const quantcastIdSubmodule = {
    * @function
    * @returns {{id: {quantcastId: string} | undefined}}}
    */
-  getId(config, consentData_) {
+  getId(config, consentData) {
     // Consent signals are currently checked on the server side.
     let fpa = storage.getCookie(QUANTCAST_FPA);
 
+    const configParams = (config && config.params) || {};
     const storageParams = (config && config.storage) || {};
 
+    emailHash = configParams.uh || '';
     cookieExpTime = storageParams.expires || DEFAULT_COOKIE_EXP_TIME;
+    globalConsentData = consentData;
 
     // Callbacks on Event Listeners won't trigger if the event is already complete so this check is required
     if (document.readyState === 'complete') {
